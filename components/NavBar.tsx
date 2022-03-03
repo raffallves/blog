dfaff15cce229b4412d781064a6b0939b984130b
raffallves <89459281+raffallves@users.noreply.github.com>
2022-03-03 11:21:05 -0300

    Set basic nav bar CSS layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,13 +5,23 @@ export default function NavBar<Any>() {
         <>
             <style jsx>{`
                 nav {
+                    display: flex;
+                    flex-flow: row wrap;
+                    justify-content: flex-end;
+                    align-content: center;
                     background: black;
                     color: white;
+                    height: 7vh;
+                    font-size: 1.2rem;
+                }
+
+                #about {
+                    padding-right: 2em;
                 }
             `}</style>
             <nav>
                 <Link href={'/about'}>
-                    <a>About</a>
+                    <a id={'about'}>About</a>
                 </Link>
             </nav>
         </>
