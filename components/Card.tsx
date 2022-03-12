e9be6bdba348c8535fe565ea1e8f426f6a58e141
raffallves <89459281+raffallves@users.noreply.github.com>
2022-03-12 16:30:19 -0300

    Add overall Card implementation

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,42 @@
-export default function Card<Any>({title, date}) {
+import Link from 'next/link'
+import Image from 'next/image'
+import Date from './Date'
+
+export default function Card<Any>({title, date, slug}) {
     return (
         <>
             <style jsx>{`
-            
+                .card {
+                    border: 2px solid black;
+                    height: 330px;
+                    width: 300px;
+                    margin: 2.5em;
+                    text-overflow: ellipsis;
+                }
+
+                .card:hover {
+                    border-color: red;
+                }
+
+                .card-content {
+                    padding: 30px;
+                }
+
+                .card-image {
+                    object-fit: contain;
+                }
+                
             `}</style>
-            <div>
-                {title}
-                {date}
+            <div className={"card"}>
+                <Image className={'card-image'} src={'/pexel.jpg'} alt={'Post image'} width={'300'} height={'80'} layout={'responsive'}/>
+                <div className={'card-content'}>
+                    <Link href={`/posts/${slug}`}>
+                        <a>
+                            <h3>{title}</h3>
+                        </a>
+                    </Link>
+                    <Date dateString={date}/>
+                </div>
             </div>
         </>
     )
