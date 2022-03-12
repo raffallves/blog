dff959ceea699b39da22b7f8e7238f8b282f15bc
raffallves <89459281+raffallves@users.noreply.github.com>
2022-03-11 22:13:27 -0300

    Create Card component initial layout

---
+++ components/Card.tsx
@@ -0,0 +1,13 @@
+export default function Card<Any>({title, date}) {
+    return (
+        <>
+            <style jsx>{`
+            
+            `}</style>
+            <div>
+                {title}
+                {date}
+            </div>
+        </>
+    )
+}
